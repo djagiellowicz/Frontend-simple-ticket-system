59d5df5f19fe7feb679f4f21726bfa8f1f19312f
Dariusz Jagiellowicz <darekjagiellowicz@gmail.com>
2018-04-29 10:00:05 +0200

    added show form on click

--- app/viewIncidents/viewIncidents.js
+++ app/viewIncidents/viewIncidents.js
@@ -16,6 +16,7 @@ angular.module('myApp.viewIncidents', ['ngRoute'])
         self.incidentsList = [];
         self.pageNumber = 0;
         self.totalNumberOfElements = 0;
+        self.showme = false;
 
 
         this.fetchIncidents = function () {
@@ -53,9 +54,23 @@ angular.module('myApp.viewIncidents', ['ngRoute'])
             self.pageNumber = self.pageNumber - 1;
             self.fetchIncidents();
         };
-        this.alert = function(){
+        this.alert = function(data){
+            var id = data.id;
+            self.showme = true;
+            $http.get(URL + '/incident/get/' + id)
+                .then(
+                    function (incident) {
+                        console.log(incident);
+                        document.getElementById("id_form").value = incident.data.object.id;
+                        document.getElementById("description_form").value = incident.data.object.description;
+                    },
+                    function () {
+                        console.log("error");
+                    }
+                );
             alert("It's alive!");
-        }
+
+        };
         self.fetchIncidents();
 
 }]);
